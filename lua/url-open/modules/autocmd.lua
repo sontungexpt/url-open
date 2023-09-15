f6d3a363dae05243d6c2b64cb3df5add48f7bc10
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-09-15 15:00:24 +0700

    fix: change the location in if else in autocmd module

--- lua/url-open/modules/autocmd.lua
+++ lua/url-open/modules/autocmd.lua
@@ -8,13 +8,7 @@ local M = {}
 
 M.setup = function(user_opts)
 	if user_opts.highlight_url.enabled then
-		if not user_opts.highlight_url.cursor_only then
-			api.nvim_create_autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
-				desc = "URL Highlighting",
-				group = api.nvim_create_augroup("HightlightAllUrl", { clear = true }),
-				callback = function() handlers.set_url_effect() end,
-			})
-		else
+		if user_opts.highlight_url.cursor_only then
 			api.nvim_create_autocmd({ "CursorMoved" }, {
 				desc = "URL Highlighting CursorMoved",
 				group = api.nvim_create_augroup("HighlightCursorUrl", { clear = true }),
@@ -22,6 +16,12 @@ M.setup = function(user_opts)
 					handlers.highlight_cursor_url(user_opts)
 					api.nvim_set_hl(0, "HighlightCursorUrl", { underline = true })
 				end,
+			})
+		else
+			api.nvim_create_autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
+				desc = "URL Highlighting",
+				group = api.nvim_create_augroup("HightlightAllUrl", { clear = true }),
+				callback = function() handlers.set_url_effect() end,
 			})
 		end
 	end
