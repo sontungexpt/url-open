ca494d4362902f65b2a05ffb9edbd98fc9dbc1da
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-09-22 09:28:03 +0700

    fix: highlight all url at startup

--- lua/url-open/modules/autocmd.lua
+++ lua/url-open/modules/autocmd.lua
@@ -19,33 +19,19 @@ M.setup = function(user_opts)
 	local highlight_url = user_opts.highlight_url
 
 	if highlight_url.all_urls.enabled then
-		local is_loaded = false
-		local cursor_hold_id = 0
-		-- TODO: Need to find a other way to enable command when open neovim
-		cursor_hold_id = api.nvim_create_autocmd({ "CursorHold" }, {
-			desc = "URL Highlighting",
-			group = api.nvim_create_augroup("HighlightAllUrlWhenOpenNeovim", { clear = true }),
-			callback = function()
-				if not is_loaded then
-					api.nvim_command("HighlightAllUrls")
-					is_loaded = true
-				else --remove autocmd after first run
-					api.nvim_del_autocmd(cursor_hold_id)
-				end
-			end,
-		})
+		api.nvim_command("HighlightAllUrls") -- Highlight all urls on startup
 
-		api.nvim_create_autocmd({ "BufEnter", "WinEnter", "VimEnter" }, {
-			desc = "URL Highlighting",
-			group = api.nvim_create_augroup("HighlightAllUrl", { clear = true }),
+		autocmd({ "BufEnter", "WinEnter" }, {
+			desc = "Highlight all urls in the buffer",
+			group = augroup("HighlightAllUrl", { clear = true }),
 			command = "HighlightAllUrls",
 		})
 	end
 
 	if highlight_url.cursor_move.enabled then
-		api.nvim_create_autocmd({ "CursorMoved" }, {
-			desc = "URL Highlighting CursorMoved",
-			group = api.nvim_create_augroup("HighlightCursorUrl", { clear = true }),
+		autocmd({ "CursorMoved" }, {
+			desc = "Highlight the url under the cursor",
+			group = augroup("HighlightCursorUrl", { clear = true }),
 			callback = function()
 				handlers.highlight_cursor_url(user_opts)
 				handlers.change_color_highlight(highlight_url.cursor_move, "HighlightCursorUrl")
