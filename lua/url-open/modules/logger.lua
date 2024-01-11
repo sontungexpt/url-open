f39c3cfeaaaad6146d8045155e09ad9592c796c1
Aaron Lichtman <aaronlichtman@gmail.com>
2024-01-11 12:36:01 -0800

    Make notifications a little less agressive
    - The title of the notifications now contains the lowercased plugin name
    - Verbiage of the success / failure notifications is cleaner (no need to
      say it's an error if the notifications framework will style it like an
      error)

--- lua/url-open/modules/logger.lua
+++ lua/url-open/modules/logger.lua
@@ -10,21 +10,21 @@ local schedule = vim.schedule
 --- @tparam string msg : The message to show
 --- @tparam[opt] table opts : The options to pass to vim.notify
 M.info = function(msg, opts)
-	schedule(function() notify(msg, levels.INFO, opts or { title = "URL OPEN INFO" }) end)
+	schedule(function() notify(msg, levels.INFO, opts or { title = "url-open" }) end)
 end
 
 --- Show warn level notification
 --- @tparam string msg : The message to show
 --- @tparam[opt] table opts : The options to pass to vim.notify
 M.warn = function(msg, opts)
-	schedule(function() notify(msg, levels.WARN, opts or { title = "URL OPEN WARNING" }) end)
+	schedule(function() notify(msg, levels.WARN, opts or { title = "url-open" }) end)
 end
 
 --- Show error level notification
 --- @tparam string msg : The message to show
 --- @tparam[opt] table opts : The options to pass to vim.notify
 M.error = function(msg, opts)
-	schedule(function() notify(msg, levels.ERROR, opts or { title = "URL OPEN ERROR" }) end)
+	schedule(function() notify(msg, levels.ERROR, opts or { title = "url-open" }) end)
 end
 
 return M
